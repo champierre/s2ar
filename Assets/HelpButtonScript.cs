331398da91528fee607a5244aa9be2be6ae32f9a
Junya Ishihara <junya@champierre.com>
2017-11-22 23:20:34 +0900

    Add Help Button.

---
+++ Assets/HelpButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void HelpButtonPressed() {
+		Debug.Log ("Help");
+	}
+
+}
