4c95d7960798a7cb5c6609a864a62324d74c1c5b
Junya Ishihara <junya@champierre.com>
2017-11-23 13:03:25 +0900

    Add Url button.

---
+++ Assets/UrlButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void UrlButtonPressed() {
+		Application.OpenURL("https://github.com/champierre/s2ar");
+	}
+}
