cef96fc03bdec81e6a8bb9bea28ea9811790e30f
Junya Ishihara <junya@champierre.com>
2017-11-23 10:43:51 +0900

    Add close button to panel.

--- Assets/HelpCloseButtonScript.cs
+++ Assets/HelpCloseButtonScript.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelpButtonScript : MonoBehaviour {
+public class HelpCloseButtonScript : MonoBehaviour {
 	[SerializeField]
 	GameObject panel;
 
 	// Use this for initialization
 	void Start () {
 	}
-
-	public void HelpButtonPressed() {
-		panel.SetActive (true);
+	
+	public void HelpButtonClosePressed() {
+		panel.SetActive (false);
 	}
 }
