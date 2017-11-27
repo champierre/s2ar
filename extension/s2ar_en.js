26df5588ef0e58992886e6e86e9f22588da1ea4d
Junya Ishihara <junya@champierre.com>
2017-11-27 21:51:14 +0900

    Add sample scratch project.

---
+++ extension/s2ar_en.js
@@ -0,0 +1,71 @@
+(function(ext) {
+  var ip = "";
+  var connected = false;
+
+  ext._shutdown = function() {};
+
+  ext._getStatus = function() {
+      return {status: 2, msg: 'Ready'};
+  };
+
+  ext.set_cube = function(x, y, z) {
+    if (!connected) {
+      ws = new WebSocket('ws://' + ip + ':3000');
+    } else {
+      ws.send("set_cube:" + x + ":" + y + ":" + z);
+    }
+    ws.onopen = function(){ connected = true; ws.send("set_cube:" + x + ":" + y + ":" + z); };
+    ws.onclose = function(){ connected = false; };
+  }
+
+  ext.set_color = function(r, g, b) {
+    if (!connected) {
+      ws = new WebSocket('ws://' + ip + ':3000');
+    } else {
+      ws.send("set_color:" + r + ":" + g + ":" + b);
+    }
+    ws.onopen = function(){ connected = true; ws.send("set_color:" + r + ":" + g + ":" + b); };
+    ws.onclose = function(){ connected = false; };
+  }
+
+  ext.reset = function() {
+    if (!connected) {
+      ws = new WebSocket('ws://' + ip + ':3000');
+    } else {
+      ws.send("reset");
+    }
+    ws.onopen = function(){ connected = true; ws.send("reset"); };
+    ws.onclose = function(){ connected = false; };
+  }
+
+  ext.set_ip = function(str) {
+    ip = str;
+  };
+
+  var lang = 'en';
+  var locale = {
+    ja: {
+      set_ip: '接続先IPを %s に設定する',
+      set_cube: 'x座標を %n 、y座標を %n 、z座標を %n にブロックを置く',
+      set_color: 'ブロックの色を r: %n g: %n b: %n に変える',
+      reset: 'リセット'
+    },
+    en: {
+      set_ip: 'Set IP address to %s',
+      set_cube: 'set cube at x: %n y: %n z: %n',
+      set_color: 'set color to r: %n g: %n b: %n',
+      reset: 'reset'
+    },
+  }
+
+  var descriptor = {
+    blocks: [
+      [' ', locale[lang].set_ip, 'set_ip', ip],
+      [' ', locale[lang].set_cube, 'set_cube', 1, 0, 1],
+      [' ', locale[lang].set_color, 'set_color', 255, 255, 255],
+      [' ', locale[lang].reset, 'reset']
+    ]
+  };
+
+  ScratchExtensions.register('Scratch2ARKit', descriptor, ext);
+})({});
